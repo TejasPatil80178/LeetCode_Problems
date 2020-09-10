3c1be394c7438fc8b6d4c793be8029641bc07d43
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-10 20:16:04 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -8,7 +8,7 @@ public:
         
         
         for(int i = 0;i<n-1;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            if(i>0 && nums[i] == nums[i-1]) continue; //avoid duplicates
             int l = i+1;
             int r = n-1;
             while(l<r){
@@ -18,9 +18,9 @@ public:
                 else{
                     v.push_back({nums[i],nums[l],nums[r]});
                     l++;
-                    while(l < r && nums[l]==nums[l-1]) l++;
+                    while(l < r && nums[l]==nums[l-1]) l++;   //avoid duplicates
                     r--;
-                    while(l<r && nums[r] == nums[r+1]) r--;
+                    while(l < r && nums[r] == nums[r+1]) r--;   //avoid duplicates
                 }
             }
         }
