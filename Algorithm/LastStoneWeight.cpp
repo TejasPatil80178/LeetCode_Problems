36927021343fa71a4d2821d46bad7dc6c91f47b8
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-22 11:30:54 +0530

    Create LastStoneWeight.cpp

---
+++ Algorithm/LastStoneWeight.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        if(n==0) return 0;
+        if(n==1) return stones[0];
+        
+        priority_queue<int> pq;
+        for(int i = 0;i<n;i++){
+            pq.push(stones[i]);
+        }
+        
+        while(!pq.empty()){
+            if(pq.size()==1) return pq.top();
+            
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            
+            if(x!=y){
+                pq.push(x-y);
+            }
+            
+        }
+        
+        return 0;
+    }
+};
