c949886221ad51b7fd9a84ca1976a0f6a0581b35
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-27 20:45:34 +0530

    Create IslandPerimeter.cpp

---
+++ Algorithm/IslandPerimeter.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int p = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j]==1){
+                    p+=4;
+                    if(i>0 && grid[i-1][j]==1) p--;
+                    if(i<n-1 && grid[i+1][j]==1) p--;
+                    if(j>0 && grid[i][j-1]==1) p--;
+                    if(j<m-1 && grid[i][j+1]==1) p--;
+                }
+            }
+        }
+        return p;
+    }
+};
